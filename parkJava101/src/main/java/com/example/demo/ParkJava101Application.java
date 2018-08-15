4870d31625cbf1f2c5d5d00a9e98ddd158913951
LAPTOP-QSHBKJ20\codeback <newbornking87@gmail.com>
2018-08-15 21:59:44 +0900

    no message

---
+++ parkJava101/src/main/java/com/example/demo/ParkJava101Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkJava101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkJava101Application.class, args);
+	}
+}
